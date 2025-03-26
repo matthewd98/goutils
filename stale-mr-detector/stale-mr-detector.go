66fb12d53c49c5580ab43cdbae0e932d72368af3
Matthew D'Addario <58401200+matthew-daddario@users.noreply.github.com>
2025-03-26 00:38:02 -0400

    delete stale branches and iterate all pages

--- stale-mr-detector/stale-mr-detector.go
+++ stale-mr-detector/stale-mr-detector.go
@@ -3,6 +3,7 @@ package main
 // Use 'go run <filepath>' to execute this file
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -18,19 +19,15 @@ import (
 )
 
 const (
-	slackChannelID             = "C##########"
-	gitLabBaseURL              = "https://gitlab.YOURDOMAIN.com/api/v4"
-	gitLabSupplyProjectID      = 2520
-	maxMergeRequestsToRetrieve = 100
-	jiraBaseURL                = "https://jira.YOURDOMAIN.com"
+	gitLabBaseURL = "https://gitlab.YOURDOMAIN.com/api/v4"
+	maxPerPage    = 100
+	jiraBaseURL   = "https://jira.YOURDOMAIN.com"
 )
 
-// Assumes that MR title has the following format "Title [ISSUE-1234]"
-// The first segment is the commit title and the last segment is The JIRA issue enclosed in bracket quotes.
-var mrTitleRegex *regexp.Regexp = regexp.MustCompile(`(.*)\s*\[([A-Z0-9]+-[0-9]+)\]`)
+var mrTitleRegex = regexp.MustCompile(`(.*)\s*\[([A-Z0-9]+-[0-9]+)\]`)
 
 func main() {
-	gitLabToken, slackToken, jiraToken, isDryRun, err := parseArgs()
+	gitLabToken, gitLabProjectID, slackToken, slackChannelID, jiraToken, isDryRun, err := parseArgs()
 	if err != nil {
 		outputErrorAndExit(err)
 	}
@@ -50,14 +47,30 @@ func main() {
 
 	slackClient := slack.New(slackToken)
 
-	mrs, err := getMergeRequests(gitLabClient)
+	// branches
+
+	branches, err := getBranches(gitLabClient, gitLabProjectID)
 	if err != nil {
 		outputErrorAndExit(err)
 	}
 
-	staleMRs := getStaleMergeRequests(mrs)
+	staleBranches := filterStaleNonProtectedBranches(branches)
 
-	expiredMRs, err := closeExpiredMergeRequests(mrs, gitLabClient, jiraClient, isDryRun)
+	err = deleteStaleBranches(staleBranches, gitLabClient, gitLabProjectID, isDryRun)
+	if err != nil {
+		outputErrorAndExit(err)
+	}
+
+	// merge requests
+
+	mrs, err := getMergeRequests(gitLabClient, gitLabProjectID)
+	if err != nil {
+		outputErrorAndExit(err)
+	}
+
+	staleMRs := filterStaleMergeRequests(mrs)
+
+	expiredMRs, err := closeExpiredMergeRequests(mrs, gitLabClient, gitLabProjectID, jiraClient, isDryRun)
 	if err != nil {
 		outputErrorAndExit(err)
 	}
@@ -69,57 +82,148 @@ func main() {
 
 	slackUserIDByGitLabUserID := getSlackUserIDsFromGitLabUserIDs(slackClient, staleMRs, expiredMRs)
 
-	inviteUsersToSlackChannel(slackClient, slackUserIDByGitLabUserID, isDryRun)
+	inviteUsersToSlackChannel(slackClient, slackUserIDByGitLabUserID, slackChannelID, isDryRun)
 
-	if err := buildAndPostSlackMessage(slackClient, staleMRs, expiredMRs, slackUserIDByGitLabUserID, isDryRun); err != nil {
+	if err := buildAndPostSlackMessage(slackClient, staleMRs, expiredMRs, slackUserIDByGitLabUserID, slackChannelID, isDryRun); err != nil {
 		outputErrorAndExit(err)
 	}
 }
 
-func parseArgs() (string, string, string, bool, error) {
+func parseArgs() (string, string, string, string, string, bool, error) {
 	args := os.Args
-	if len(args) < 4 {
-		return "", "", "", false, fmt.Errorf("missing args. Usage: [gitlabtoken] [slacktoken] [jiratoken] [dryrun]")
+	if len(args) < 6 {
+		return "", "", "", "", "", false, errors.New("missing args. Usage: [gitlab token] [gitlab project id] [slack token] [slack channel id] [jira token] [dryrun]")
 	}
 
 	gitLabToken := args[1]
-	slackToken := args[2]
-	jiraToken := args[3]
+	gitLabProjectID := args[2]
+	slackToken := args[3]
+	slackChannelID := args[4]
+	jiraToken := args[5]
 
 	isDryRun := false
-	if len(args) == 5 {
-		isDryRun, _ = strconv.ParseBool(args[4])
+	if len(args) == 7 {
+		isDryRun, _ = strconv.ParseBool(args[6])
 	}
 	if isDryRun {
 		fmt.Println("dryrun option enabled, GitLab MRs will not be updated and Slack messages will not be posted.")
 	}
 
-	return gitLabToken, slackToken, jiraToken, isDryRun, nil
+	return gitLabToken, gitLabProjectID, slackToken, slackChannelID, jiraToken, isDryRun, nil
 }
 
-// Get merge requests that haven't been updated for over 2 months
-func getMergeRequests(client *gitlab.Client) ([]*gitlab.MergeRequest, error) {
-	options := &gitlab.ListProjectMergeRequestsOptions{
-		State:       gitlab.String("opened"),
-		ListOptions: gitlab.ListOptions{PerPage: maxMergeRequestsToRetrieve},
+// Get all branches
+func getBranches(client *gitlab.Client, gitLabProjectID string) ([]*gitlab.Branch, error) {
+	var branches []*gitlab.Branch
+
+	options := &gitlab.ListBranchesOptions{
+		ListOptions: gitlab.ListOptions{PerPage: maxPerPage},
 	}
 
-	mergeRequests, resp, err := client.MergeRequests.ListProjectMergeRequests(gitLabSupplyProjectID, options)
-	if err != nil {
-		return nil, fmt.Errorf("gitlab - http client error: %w", err)
+	for {
+		branchesInPage, resp, err := client.Branches.ListBranches(gitLabProjectID, options)
+		if err != nil {
+			return nil, fmt.Errorf("gitlab - http client error: %w", err)
+		}
+		if resp.StatusCode >= 400 {
+			return nil, fmt.Errorf("gitlab - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
+		}
+
+		branches = append(branches, branchesInPage...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+
+		options.ListOptions.Page = resp.NextPage
 	}
-	if resp.StatusCode > 400 {
-		return nil, fmt.Errorf("gitlab - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
+
+	fmt.Printf("Branches found: %d\n", len(branches))
+	fmt.Printf("Name of branches: %s\n", extractBranchNames(branches))
+
+	return branches, nil
+}
+
+// Filter non-protected branches that haven't been updated for over 6 months
+func filterStaleNonProtectedBranches(branches []*gitlab.Branch) []*gitlab.Branch {
+	staleBranches := []*gitlab.Branch{}
+	sixMonthsAgo := time.Now().AddDate(0, -6, 0)
+	for _, b := range branches {
+		if !b.Protected && b.Commit.CommittedDate.Before(sixMonthsAgo) {
+			staleBranches = append(staleBranches, b)
+		}
+	}
+
+	fmt.Printf("stale branches found: %d\n", len(staleBranches))
+	fmt.Printf("name of stale non-protected branches: %s\n", extractBranchNames(staleBranches))
+
+	return staleBranches
+}
+
+func extractBranchNames(branches []*gitlab.Branch) string {
+	names := []string{}
+	for _, b := range branches {
+		names = append(names, b.Name)
+	}
+
+	return strings.Join(names, ",")
+}
+
+func deleteStaleBranches(staleBranches []*gitlab.Branch, gitlabClient *gitlab.Client, gitLabProjectID string, isDryRun bool) error {
+	if isDryRun {
+		return nil
+	}
+
+	for _, b := range staleBranches {
+		resp, err := gitlabClient.Branches.DeleteBranch(gitLabProjectID, b.Name)
+		if err != nil {
+			return fmt.Errorf("gitlab - http client error: %w", err)
+		}
+		if resp.StatusCode >= 400 {
+			return fmt.Errorf("gitlab - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
+		}
+	}
+
+	fmt.Printf("deleted %d stale branches", len(staleBranches))
+
+	return nil
+}
+
+// Get all merge requests
+func getMergeRequests(client *gitlab.Client, gitLabProjectID string) ([]*gitlab.MergeRequest, error) {
+	var mergeRequests []*gitlab.MergeRequest
+
+	options := &gitlab.ListProjectMergeRequestsOptions{
+		State:       gitlab.String("opened"),
+		ListOptions: gitlab.ListOptions{PerPage: maxPerPage},
+	}
+
+	for {
+		mergeRequestsInPage, resp, err := client.MergeRequests.ListProjectMergeRequests(gitLabProjectID, options)
+		if err != nil {
+			return nil, fmt.Errorf("gitlab - http client error: %w", err)
+		}
+		if resp.StatusCode >= 400 {
+			return nil, fmt.Errorf("gitlab - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
+		}
+
+		mergeRequests = append(mergeRequests, mergeRequestsInPage...)
+
+		if resp.NextPage == 0 {
+			break
+		}
+
+		options.ListOptions.Page = resp.NextPage
 	}
 
 	fmt.Printf("MRs found: %d\n", len(mergeRequests))
-	fmt.Printf("internal IDs of MRs: %s\n", extractMergeRequestIIDs(mergeRequests))
+	fmt.Printf("internal ID of MRs: %s\n", extractMergeRequestIIDs(mergeRequests))
 
 	return mergeRequests, nil
 }
 
-// Get merge requests that haven't been updated for over 2 months
-func getStaleMergeRequests(mrs []*gitlab.MergeRequest) []*gitlab.MergeRequest {
+// Filter merge requests that haven't been updated for over 2 months
+func filterStaleMergeRequests(mrs []*gitlab.MergeRequest) []*gitlab.MergeRequest {
 	staleMRs := []*gitlab.MergeRequest{}
 	twoMonthsAgo := time.Now().AddDate(0, -2, 0)
 	for _, mr := range mrs {
@@ -128,13 +232,14 @@ func getStaleMergeRequests(mrs []*gitlab.MergeRequest) []*gitlab.MergeRequest {
 		}
 	}
 
+	fmt.Printf("stale MRs found: %d\n", len(staleMRs))
 	fmt.Printf("internal IDs of stale MRs: %s\n", extractMergeRequestIIDs(staleMRs))
 
 	return staleMRs
 }
 
 // Close merge requests that haven't been updated for over 3 months or whose associated JIRA issue is closed
-func closeExpiredMergeRequests(mrs []*gitlab.MergeRequest, gitlabClient *gitlab.Client, jiraClient *jira.Client, isDryRun bool) ([]*gitlab.MergeRequest, error) {
+func closeExpiredMergeRequests(mrs []*gitlab.MergeRequest, gitlabClient *gitlab.Client, gitLabProjectID string, jiraClient *jira.Client, isDryRun bool) ([]*gitlab.MergeRequest, error) {
 	expiredMRs := []*gitlab.MergeRequest{}
 	threeMonthsAgo := time.Now().AddDate(0, -3, 0)
 	for _, mr := range mrs {
@@ -155,7 +260,7 @@ func closeExpiredMergeRequests(mrs []*gitlab.MergeRequest, gitlabClient *gitlab.
 				fmt.Printf("JIRA issue ID attached to MR does not exist: %d. Skipping status check.\n", mr.IID)
 				continue
 			}
-			if resp != nil && resp.StatusCode > 400 {
+			if resp != nil && resp.StatusCode >= 400 {
 				return nil, fmt.Errorf("jira - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
 			}
 			return nil, fmt.Errorf("jira - http client error: %w", err)
@@ -166,7 +271,8 @@ func closeExpiredMergeRequests(mrs []*gitlab.MergeRequest, gitlabClient *gitlab.
 		}
 	}
 
-	fmt.Printf("internal IDs of expired MRs: %s\n", extractMergeRequestIIDs(expiredMRs))
+	fmt.Printf("expired MRs found: %d\n", len(expiredMRs))
+	fmt.Printf("internal ID of expired MRs: %s\n", extractMergeRequestIIDs(expiredMRs))
 
 	if isDryRun {
 		return expiredMRs, nil
@@ -176,11 +282,11 @@ func closeExpiredMergeRequests(mrs []*gitlab.MergeRequest, gitlabClient *gitlab.
 		updateMergeRequestOptions := &gitlab.UpdateMergeRequestOptions{
 			StateEvent: gitlab.String("close"),
 		}
-		_, resp, err := gitlabClient.MergeRequests.UpdateMergeRequest(gitLabSupplyProjectID, mr.IID, updateMergeRequestOptions)
+		_, resp, err := gitlabClient.MergeRequests.UpdateMergeRequest(gitLabProjectID, mr.IID, updateMergeRequestOptions)
 		if err != nil {
 			return nil, fmt.Errorf("gitlab - http client error: %w", err)
 		}
-		if resp.StatusCode > 400 {
+		if resp.StatusCode >= 400 {
 			return nil, fmt.Errorf("gitlab - invalid request. Status code: %s. Body: %s", resp.Status, extractResponseBody(resp.Response))
 		}
 	}
@@ -235,7 +341,7 @@ func getSlackUserIDsFromGitLabUserIDs(client *slack.Client, staleMRs, expiredMRs
 	return slackUserIDByGitLabUserID
 }
 
-func inviteUsersToSlackChannel(client *slack.Client, slackUserIDByGitLabUserID map[string]string, isDryRun bool) {
+func inviteUsersToSlackChannel(client *slack.Client, slackUserIDByGitLabUserID map[string]string, slackChannelID string, isDryRun bool) {
 	slackUserIDs := []string{}
 	for _, slackUserID := range slackUserIDByGitLabUserID {
 		slackUserIDs = append(slackUserIDs, slackUserID)
@@ -254,30 +360,26 @@ func inviteUsersToSlackChannel(client *slack.Client, slackUserIDByGitLabUserID m
 	}
 }
 
-func buildAndPostSlackMessage(client *slack.Client, staleMRs, expiredMRs []*gitlab.MergeRequest, slackUserIDByGitLabUserID map[string]string, isDryRun bool) error {
+func buildAndPostSlackMessage(client *slack.Client, staleMRs, expiredMRs []*gitlab.MergeRequest, slackUserIDByGitLabUserID map[string]string, slackChannelID string, isDryRun bool) error {
 	blocks := []slack.Block{}
 	if len(staleMRs) > 0 {
+		//nolint:lll
 		blocks = append(blocks, slack.NewSectionBlock(slack.NewTextBlockObject(slack.MarkdownType, "*Stale MRs (more than 2 months old without any updates):*\nThese MRs will be automatically closed in 1 month if they aren't updated", false, false), nil, nil))
 		text := ""
 		for _, mr := range staleMRs {
 			title, jiraURL := parseMRTitle(mr.Title)
-			slackUserID, ok := slackUserIDByGitLabUserID[mr.Author.Username]
-			if !ok {
-				slackUserID = "Unknown"
-			}
+			slackUserID := getSlackUserID(slackUserIDByGitLabUserID, mr)
 			text += fmt.Sprintf(":alarm_clock: <%s|!%d %s> %s - <@%s>\n", mr.WebURL, mr.IID, title, jiraURL, slackUserID)
 		}
 		blocks = append(blocks, slack.NewSectionBlock(slack.NewTextBlockObject(slack.MarkdownType, text, false, false), nil, nil))
 	}
 	if len(expiredMRs) > 0 {
+		//nolint:lll
 		blocks = append(blocks, slack.NewSectionBlock(slack.NewTextBlockObject(slack.MarkdownType, "*MRs that have been closed (due to staleness or the associated JIRA issue being closed):*", false, false), nil, nil))
 		text := ""
 		for _, mr := range expiredMRs {
 			title, jiraURL := parseMRTitle(mr.Title)
-			slackUserID, ok := slackUserIDByGitLabUserID[mr.Author.Username]
-			if !ok {
-				slackUserID = "Unknown"
-			}
+			slackUserID := getSlackUserID(slackUserIDByGitLabUserID, mr)
 			text += fmt.Sprintf(":x: <%s|!%d %s> %s - <@%s>\n", mr.WebURL, mr.IID, title, jiraURL, slackUserID)
 		}
 		blocks = append(blocks, slack.NewSectionBlock(slack.NewTextBlockObject(slack.MarkdownType, text, false, false), nil, nil))
@@ -293,6 +395,14 @@ func buildAndPostSlackMessage(client *slack.Client, staleMRs, expiredMRs []*gitl
 	}
 
 	return nil
+}
+
+func getSlackUserID(slackUserIDByGitLabUserID map[string]string, mr *gitlab.MergeRequest) string {
+	slackUserID, ok := slackUserIDByGitLabUserID[mr.Author.Username]
+	if !ok {
+		slackUserID = "Unknown"
+	}
+	return slackUserID
 }
 
 // Extracts the title and the JIRA issue
